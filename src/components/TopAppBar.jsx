99b49775bde6799c2c59fbfeaeba4544904675b5
l <aksahy.koulu@example.com>
2021-05-14 20:16:41 +0200

    removed material ui

--- src/components/TopAppBar.jsx
+++ src/components/TopAppBar.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import {AppBar,Toolbar,IconButton, Typography,Button,} from '@material-ui/core'
-// import {Menu} from '@material-ui/icons/Menu'
+
 
 export default function TopAppBar() {
     
     return (
         <div>
-            <AppBar position="static">
-                 <Toolbar>
-                    <IconButton edge="start"  color="inherit" aria-label="menu">
-                     
-                    </IconButton> 
-                    <Typography variant="h6" >
-                    News
-                    </Typography>
-                    <Button color="inherit">Login</Button> 
-                </Toolbar> 
-            </AppBar>
         </div>
     )
     }
